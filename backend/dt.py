a7d91bf0b22c25a7ee086073815cb667eff4a74e
David <155638668+dcg567@users.noreply.github.com>
2025-07-31 20:26:08 +0100

    Update comments

--- backend/dt.py
+++ backend/dt.py
@@ -4,35 +4,35 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
 
 def train_and_evaluate_decision_tree(csv_path):
-    # Load your dataset
+    #Load dataset
     dataset = pd.read_csv("/Users/dcg/Desktop/bodyfat_predictor_app/data/bf_clean.csv")
 
-    # Split into features and target
+    #Split into features and target
     targets = dataset['BodyFat']
     features = dataset.drop(['BodyFat'], axis=1)
 
-    # Add more columns to include in features (if necessary)
+    #Add more columns to include in features
     additional_columns = ['Weight', 'Age', 'Height']
     features = pd.concat([features, dataset[additional_columns]], axis=1)
 
-    # Split the dataset into training and testing sets
+    #Split the dataset into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(features, targets, test_size=0.2, random_state=42)
 
-    # Initialize the Decision Tree regressor
+    #Initialize the Decision Tree regressor
     decision_tree = DecisionTreeRegressor(random_state=42)
 
-    # Fit the model
+    #Fit the model
     decision_tree.fit(X_train, y_train)
 
-    # Make predictions
+    #Make predictions
     y_pred = decision_tree.predict(X_test)
 
-    # Calculate the percentage accuracy for each prediction
+    #Calculate the percentage accuracy for each prediction
     percentage_accuracies = 100 - abs((y_pred - y_test) / y_test) * 100
 
-    # Print the average percentage accuracy
+    #Print the average percentage accuracy
     average_percentage_accuracy = percentage_accuracies.mean()
     print("Average Percentage Accuracy:", average_percentage_accuracy)
 
-    # Return the trained model if you want to save or use it later
+    #Return the trained model if you want to save or use it later
     return decision_tree
